8d003486effdbae00014810f80090cbc95dde719
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2023-09-27 11:05:00 +0500

    admin middle ware
    created middle ware for addmin

--- dist/routes/authRoutes.js
+++ dist/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { loginController, registerController, testController } from "../controllers/authController.js";
-import { requireSignIn } from "../middlewares/authMiddleware.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 // routing
 // Register || Method Post
@@ -8,5 +8,5 @@ router.post("/register", registerController);
 // login || method post
 router.post("/login", loginController);
 //test routes
-router.get("/test", requireSignIn, testController);
+router.get("/test", requireSignIn, isAdmin, testController);
 export default router;
